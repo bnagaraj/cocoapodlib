901cd0f9ea4916f2d5fd131bca7649b723001ab0
Nagaraj Balan <nagaraj.balan@impigertech.com>
2017-01-20 15:55:02 +0530

    Initial Commit

---
+++ nagulib/nagulib/nagulib.h
@@ -0,0 +1,19 @@
+//
+//  nagulib.h
+//  nagulib
+//
+//  Created by Nagaraj on 20/01/17.
+//  Copyright © 2017 Nagaraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for nagulib.
+FOUNDATION_EXPORT double nagulibVersionNumber;
+
+//! Project version string for nagulib.
+FOUNDATION_EXPORT const unsigned char nagulibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <nagulib/PublicHeader.h>
+
+
